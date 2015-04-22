a992a72aadb67387f0fc2e2315748ae5fd334ac7
Jorge Timón <jtimon@jtimon.cc>
2015-04-22 09:08:32 +0200

    SQUASHME: MOVEONLY: Rename policyestimator to policy/fees

--- src/test/policyestimator_tests.cpp
+++ src/test/policyestimator_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "policyestimator.h"
+#include "policy/fees.h"
 #include "txmempool.h"
 #include "uint256.h"
 #include "util.h"
