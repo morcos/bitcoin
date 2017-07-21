96b89348b39a04b2653a52708d3f09ff4bb892cb
Alex Morcos <morcos@chaincode.com>
2017-07-21 15:41:48 -0400

    Segsignal compatible UASF

--- qa/rpc-tests/p2p-versionbits-warning.py
+++ qa/rpc-tests/p2p-versionbits-warning.py
@@ -74,7 +74,7 @@ class VersionBitsWarningTest(BitcoinTestFramework):
         # Open and close to create zero-length file
         with open(self.alert_filename, 'w', encoding='utf8') as _:
             pass
-        self.extra_args = [["-debug", "-logtimemicros=1", "-alertnotify=echo %s >> \"" + self.alert_filename + "\""]]
+        self.extra_args = [["-debug", "-logtimemicros=1", "-bip9params=segwit:0:0", "-alertnotify=echo %s >> \"" + self.alert_filename + "\""]]
         self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, self.extra_args)
 
     # Send numblocks blocks via peer with nVersionToUse set.
